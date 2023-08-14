228a29cc2f9d78581b06e049477e73c169713b86
atiehghane60 <atiehghane60@gmail.com>
2023-08-15 00:12:42 +0330

    :up: (config app): update app configuration

---
+++ src/pages/errors/notFound/404.tsx
@@ -0,0 +1,23 @@
+// libraries
+import React from 'react';
+import { Link } from 'react-router-dom';
+// assets and styles
+import notFoundImage from 'assets/404.png';
+import styles from './styles.module.scss';
+
+const NotFound = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Oops! Page Not Found</h1>
+      <h4 className={styles.subtitle} >404</h4>
+      <p className={styles.message}>
+        We're sorry, but the page you are looking for can't be found...
+      </p>
+      <Link to="/" className={styles['home-link']}>
+        Go back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/pages/index.ts
@@ -0,0 +1,21 @@
+import Search from './Search';
+import NotFound from './errors/notFound/404';
+
+const Pages = [
+  {
+    id: 0,
+    title:"Page Not Found",
+    component: NotFound,
+    path: '*',
+    exact: true,
+  },
+  {
+    id: 1,
+    title:"Search",
+    component: Search,
+    path: '/',
+    exact: true,
+  },
+];
+
+export default Pages;
